cd1841b6013f66b29a92493c6083a00fd2f6f7d0
Martin Davis <martin_davis3@yahoo.ca>
2020-06-03 10:29:25 -0600

    add context to home controller for name reflection

--- HikingClubTripList/Controllers/HomeController.cs
+++ HikingClubTripList/Controllers/HomeController.cs
@@ -6,20 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HikingClubTripList.Models;
+using HikingClubTripList.Data;
 
 namespace HikingClubTripList.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ClubContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ClubContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var loggedInMember = LoggedInMember();
+            if (loggedInMember == null)
+            {
+                ViewData["LoggedInMemberName"] = "Guest";
+            }
+            ViewData["LoggedInMemberName"] = loggedInMember.Name;
             return View();
         }
 
@@ -32,6 +41,13 @@ namespace HikingClubTripList.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private Member LoggedInMember()
+        {
+            var member = _context.Members
+                .FirstOrDefault(m => m.IsLoggedIn);
+            return member;
         }
     }
 }
