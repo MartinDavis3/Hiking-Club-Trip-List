ccc414466fc98b5aa22cead12347da261ec95047
Martin Davis <martin_davis3@yahoo.ca>
2020-06-08 12:53:15 -0600

    add Regex and error messages to trip model

--- HikingClubTripList/Models/Trip.cs
+++ HikingClubTripList/Models/Trip.cs
@@ -20,23 +20,25 @@ namespace HikingClubTripList.Models
 
         [StringLength(30, MinimumLength = 5)]
         [Required]
+        [RegularExpression(@"^[A-Z]+.*[^\.]$", ErrorMessage = "The title must start with a capital letter. Please, do not terminate the title with a full stop.")]
         public string Title { get; set; }
 
         [Required]
         public Level Level { get; set; }
 
         [Display(Name = "Distance (km)")]
-        [Range(0, 100)]
+        [Range(0, 100, ErrorMessage = "Distance must be between 0 and 100 km.")]
         public double? Distance { get; set; }
 
-        [Range(-5000, 5000)]
+        [Range(-5000, 5000, ErrorMessage = "Elevation gain must be between -5 000 and 5 000 m.")]
         [Display(Name = "Elevation Gain (m)")]
         public double? ElevationGain { get; set; }
 
         [StringLength(800)]
+        [RegularExpression(@"^[A-Z]+.*", ErrorMessage = "The description must start with a capital letter.")]
         public string Description { get; set; }
 
-        [Range(2, 12)]
+        [Range(2, 12, ErrorMessage = "The number of participants must be between 2 and 12.")]
         [Display(Name = "Maximum Participants")]
         [Required]
         public int MaxParticipants { get; set; }
