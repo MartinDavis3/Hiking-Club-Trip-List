511cb651a4594f734a545895119c1c6751af5cd4
Martin Davis <martin_davis3@yahoo.ca>
2020-06-08 21:37:29 -0600

    complete testing

--- HikingClubTripList/Models/Trip.cs
+++ HikingClubTripList/Models/Trip.cs
@@ -18,9 +18,9 @@ namespace HikingClubTripList.Models
         [Required]
         public DateTime Date { get; set; }
 
-        [StringLength(30, MinimumLength = 5)]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "The title must be between 5 and 30 characters long, inclusive.")]
         [Required]
-        [RegularExpression(@"^[A-Z]+.*[^\.]$", ErrorMessage = "The title must start with a capital letter. Please, do not terminate the title with a full stop.")]
+        [RegularExpression(@"^[A-Z]+.*[^\.\s]$", ErrorMessage = "The title must start with a capital letter, have no leading or trailing spaces and no terminating full stop.")]
         public string Title { get; set; }
 
         [Required]
@@ -34,8 +34,8 @@ namespace HikingClubTripList.Models
         [Display(Name = "Elevation Gain (m)")]
         public double? ElevationGain { get; set; }
 
-        [StringLength(800)]
-        [RegularExpression(@"^[A-Z]+.*", ErrorMessage = "The description must start with a capital letter.")]
+        [StringLength(800, ErrorMessage = "The description must be a maximum of 800 characters long, including spaces.")]
+        [RegularExpression(@"^[A-Z]+.*[^\s]$", ErrorMessage = "The description must start with a capital letter and have no leading or trailing spaces.")]
         public string Description { get; set; }
 
         [Range(2, 12, ErrorMessage = "The number of participants must be between 2 and 12.")]
--- Readme.md
+++ Readme.md
@@ -1,3 +1,15 @@
+
+![Club Logo](/HikingClubTripList/wwwroot/img/logo.png)
+
+# Wandering Heights Hiking Club - Trip Pages
+
+(N.B. This is a fictitious club for the purposes of a webpages project)
+
+## The Trip Signup Webpages.
+
+Access to the trip signup pages is restricted to members. Login credentials are supplied to members by the website coordinator.
+
+
 # Hiking Club Trip List
 
 ## The Project:
@@ -9,5 +21,7 @@ Notes:
 It is not the intention to create a full club website, just the pages required to handle trip creation and signup.
 
 A simulated, not a real, login will be used, as creating a proper login system is not the aim of the project. However, it is necessary to simulate login to demonstrate trip signup for different users.
+
+
 
 ## ***UNDER CONSTRUCTION***
